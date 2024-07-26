7315458de77acd33e02a55969c85d4c9b769a568
Unnimaya T <Unnimaya.T@godigit.com>
2024-07-26 09:38:40 +0530

    uc3-Editcontact

--- AddAddress.java
+++ AddAddress.java
@@ -5,4 +5,12 @@ public class AddAddress {
     void addContact(CreateContact contact){
         contacts.add(contact);
     }
+    CreateContact searchName(String editName){
+        for(int i=0;i<contacts.size();i++){
+            if(contacts.get(i).firstname.equals(editName)){
+                return  contacts.get(i);
+            }
+        }
+        return null;
+    }
 }
--- AddressBook.java
+++ AddressBook.java
@@ -45,11 +45,75 @@ public class AddressBook {
                     addAddress.addContact(contact);
                     break;
                 case 2:
+                    System.out.println("Enter the contact name to edit");
+                    String editName= scannerobject.nextLine();
+                    AddAddress objectaddaddress=new AddAddress();
+                    CreateContact oldContact=objectaddaddress.searchName(editName);
+                    System.out.println("Which field you want to edit");
+
+                    System.out.println("1.First Name");
+                    System.out.println("2.Last Name");
+                    System.out.println("3.Address");
+                    System.out.println("4.City");
+                    System.out.println("5.State");
+                    System.out.println("6.Zip");
+                    System.out.println("7.Phone Number");
+                    System.out.println("8.Email");
+                    System.out.println("9.Exit");
+                    int editingchoice;
+                    do{
+                        System.out.println("Enter the choice");
+                        editingchoice=scannerobject.nextInt();
+                        switch (editingchoice){
+                            case 1:
+                                System.out.println("Enter the new First Name");
+                                String newFirstname=scannerobject.nextLine();
+                                oldContact.setFirstname(newFirstname);
+                                break;
+                            case 2:
+                                System.out.println("Enter the new Last Name");
+                                String newLastname=scannerobject.nextLine();
+                                oldContact.setLastname(newLastname);
+                                break;
+                            case 3:
+                                System.out.println("Enter the new Address");
+                                String newAddress=scannerobject.nextLine();
+                                oldContact.setAddress(newAddress);
+                                break;
+                            case 4:
+                                System.out.println("Enter the new City");
+                                String newCity=scannerobject.nextLine();
+                                oldContact.setCity(newCity);
+                                break;
+                            case 5:
+                                System.out.println("Enter the new state");
+                                String newstate=scannerobject.nextLine();
+                                oldContact.setState(newstate);
+                                break;
+                            case 6:
+                                System.out.println("Enter the new Zip");
+                                int newZip=scannerobject.nextInt();
+                                oldContact.setZip(newZip);
+                                break;
+                            case 7:
+                                System.out.println("Enter the new phone number");
+                                String newPhone=scannerobject.nextLine();
+                                oldContact.setPhoneno(newPhone);
+                                break;
+                            case 8:
+                                System.out.println("Enter the new email");
+                                String newEmail=scannerobject.nextLine();
+                                oldContact.setEmail(newEmail);
+                                break;
+                            case 9:
+                                System.out.println("Exiting");
+                                break;
+                            default:
+                                System.out.println("Invalid Input!");
+                        }
+                    }while(editingchoice!=9);
                     break;
-                case 3:
-                    break;
-                case 4:
-                    break;
+                 
                 case 5:
                     System.out.println("Exiting");
                     break;
